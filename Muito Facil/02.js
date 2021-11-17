53fad33738c9107a47d3796df74fd32acc2fbacd
Vinicius dos Santos Moreira <56438538+Vsmee@users.noreply.github.com>
2021-11-17 09:33:00 -0300

    Update 02.js

--- Muito Facil/02.js
+++ Muito Facil/02.js
@@ -1,19 +1,16 @@
 /*
     https://edabit.com/challenge/Q3n42rEWanZSTmsJm
-
+    Create a function that takes an array of numbers and return both the minimum and maximum numbers, in that order.
 */
 
 function calcAge(age){
-
     dias = 0
-
     for (let index = 0; index < age; index++) {
         if(age % 4 === 0){
             dias += 365
         }else{
             dias += 365
         }
-
     }
     return dias
 }
